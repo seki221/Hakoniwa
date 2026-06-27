c0e3e2d4c56f89bf27a313206f48d9af6bccbd2e
seki221 <pawls00.bugaboo@icloud.com>
2026-06-28 08:43:42 +0900

    add: blue meshStandardMaterial

--- src/components/Simulation/World.tsx
+++ src/components/Simulation/World.tsx
@@ -7,14 +7,16 @@ export default function World() {
   return (
     <div style={{ height: '500px', background: '#f0f0f0' }}>
       <Canvas camera={{ position: [5, 5, 5], fov: 50 }}>
-        {/* 環境設定（光とカメラ操作） */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
         <OrbitControls />
-
-        {/* 分離したコンポーネントを配置 */}
         <Field />
-        <Amoeba />
+        <Amoeba initialPosition={[0, 0.5, 0]} />
+
+        <mesh position={[5, 0.5, -5]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="blue" />
+        </mesh>
       </Canvas>
     </div>
   );
