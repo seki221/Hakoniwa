4e1c48a25564a75a08a4e8f6f244052782c2d976
seki221 <pawls00.bugaboo@icloud.com>
2026-06-27 13:50:37 +0900

    create: World.tsx world_setting

---
+++ src/components/Simulation/World.tsx
@@ -0,0 +1,21 @@
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Field from './Field';
+import Amoeba from './Amoeba';
+
+export default function World() {
+  return (
+    <div style={{ height: '500px', background: '#f0f0f0' }}>
+      <Canvas camera={{ position: [5, 5, 5], fov: 50 }}>
+        {/* 環境設定（光とカメラ操作） */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} />
+        <OrbitControls />
+
+        {/* 分離したコンポーネントを配置 */}
+        <Field />
+        <Amoeba />
+      </Canvas>
+    </div>
+  );
+}
