d1e3bae7edb51f71c87af883abd82793000bdb7f
seki221 <pawls00.bugaboo@icloud.com>
2026-06-27 13:43:13 +0900

    add: components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-// import { useState } from 'react'
+import World from './components/Simulation/World';
 
-import { Canvas } from '@react-three/fiber'
-import * as THREE from 'three'
-export default function World() {
-  const camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 1;
+export default function App() {
   return (
-    <div style={{ height: '500px' }}>
-      <Canvas>
-        {/* ここが「世界」になります */}
-        <ambientLight />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color="orange" />
-
-
-        </mesh>
-      </Canvas>
+    <div>
+      <World />
     </div>
-  )
+  );
 }
