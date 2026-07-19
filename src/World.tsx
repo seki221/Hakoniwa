8abaf2e5d38d2adcebecf12dd51535ad7745f3f6
seki221 <pawls00.bugaboo@icloud.com>
2026-07-19 16:06:32 +0900

    add: 雲と空追加

--- src/World.tsx
+++ src/World.tsx
@@ -1,18 +1,45 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import Field from './Field';
+import { Sky, Cloud, OrbitControls } from "@react-three/drei";
+import { Field } from './Field';
+// import { Stars } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
 import SimulationScene from './scene/SimulationScene';
 
 export default function World() {
   return (
     <div style={{ height: '500px', background: '#f0f0f0' }}>
-      <Canvas camera={{ position: [5, 5, 5], fov: 50 }}>
+      <Canvas
+          shadows
+          camera={{
+            position: [5, 5, 5],
+            fov: 50,
+            near: 0.1,
+            far: 2000,
+          }}
+        >
         {/* 環境設定（光とカメラ操作） */}
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
         <OrbitControls />
-
-        <Field />
+        {/* 空 */}
+        <Sky
+          distance={450000}
+          sunPosition={[5, 1, 8]}
+          inclination={0}
+          azimuth={0.25}
+        />
+        {/* 雲 */}
+        <Cloud
+          position={[0, 15, -20]}
+          bounds={[10, 2, 2]}
+          opacity={0.5}
+          speed={0.4}
+          segments={20}
+        />
+          {/* 地面 */}
+        <Physics gravity={[0, -9.81, 0]}>
+          <Field />
+        </Physics>
         <SimulationScene />
       </Canvas>
     </div>
