d3e3d8099d9aab4330ca91df3fff10464a31f9bc
seki221 <pawls00.bugaboo@icloud.com>
2026-07-19 16:05:44 +0900

    add:地面に草追加

--- src/Field.tsx
+++ src/Field.tsx
@@ -1,7 +1,19 @@
-import { Grid } from '@react-three/drei';
+import * as THREE from "three"
+import { useTexture } from "@react-three/drei"
+import { CuboidCollider, RigidBody} from "@react-three/rapier"
+import grass from "./assets/grass.png"
 
-export default function Field() {
+export function Field(){
+  const texture = useTexture(grass)
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
   return (
-    <Grid infiniteGrid sectionColor="blue" cellColor="red" />
-  );
+    <RigidBody type="fixed" colliders={false}>
+      <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+      </mesh>
+      <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
+    </RigidBody>
+  )
 }
